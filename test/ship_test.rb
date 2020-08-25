ba39a28cfc450f48a80bcda9669c18aba61b93eb
Brian Liu <brian.b.liu@gmail.com>
2020-08-25 10:45:25 -0600

    Create ship class and test.

---
+++ test/ship_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def test_it_exists
+    ship = Ship.new
+
+    assert_instance_of Ship, ship
+  end
+end
