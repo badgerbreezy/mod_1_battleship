60ede82361c772641ce091575bf081d77cda2e59
Brian Liu <brian.b.liu@gmail.com>
2020-08-28 21:12:52 -0600

    Created test for consecutive column coordinates.

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
+require './lib/ship'
+
 class Board
-  attr_reader :cells_hash
+  attr_reader :cells_hash, :ord_array, :ship
 
   def initialize
     @cells_hash = {}
+    #@ord_array = []
   end
 
   def cells
@@ -17,19 +20,86 @@ class Board
     end
     @cells_hash
   end
-
+# ---------helper methods -------
   def valid_coordinate?(coordinate)
     cells.value?(cells[coordinate])
   end
 
   def valid_placement?(ship, ship_location)
+    #ship length
+    if length_difference(ship, ship_location) == true
+      return true
+    elsif length_difference(ship, ship_location) == false
+      return false
+    end
+      #consecutive coordinates
+
+
+    #diagonal
+
+
+  end
+
+  def length_difference(ship, ship_location)
     if ship.length != ship_location.count
       return false
     elsif ship.length == ship_location.count
       return true
-    elsif ship_location
     end
   end
 
+  def consecutive_column_coordinates(ship, ship_location)
+    #row__letters_pick = ship_location.map do |location|
+      #location[0]
+    #end
+    column_numbers_pick = ship_location.map do |location|
+      location[1]
+    end
+    column_numbers_test = column_numbers_pick.map do |n|
+      n.to_i
+    end
+    if column_numbers_test != column_numbers_test.sort
+      false
+    elsif column_numbers_test == column_numbers_test.sort
+      true
+    end
 
+
+
+
+
+    #column_numbers_test.each_cons(ship.length) do |number|
+      #p number
+    #end
+
+
+
+  #.ord
+  #.each_cons
+  end
+
+
+  #def convert_coordinates(ship, ship_location) # must get result of calling ordinal_difference for columns or for rows
+    #ship_location.map do |location|
+      #location_split = location.split('')
+      #location_split.map do |letter|
+        #@ord_array << letter.ord
+      #end
+    #end
+  #end
+#
+  ##def ord_array_slice
+    ##@ord_array   .each_slice(2).to_a
+  ##end
+#
+  #def ordinal_difference_column # helper function for consecutive_coordinates
+#
+    #if ord_array[2][1] - ord_array[1][1] == 1 && ord_array[1][1] - ord_array[0][1] == 1
+      #true
+    #elsif ord_array[1][1] - ord_array[0][1] == 1
+      #true
+    #else
+      #false
+    #end
+  #end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -35,20 +35,63 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_coordinate?('A22')
   end
 
+  def test_length_difference
+    board = Board.new
+    cruiser = Ship.new('Cruiser', 3)
+    submarine = Ship.new('Submarine', 2)
+  end
+
   def test_valid_placement?
     board = Board.new
     cruiser = Ship.new('Cruiser', 3)
     submarine = Ship.new('Submarine', 2)
-#binding.pry
+
     #same length:
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
-    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+  #assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
 
-    #consecutive coordinates
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
-    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    assert_equal false, board.valid_placement?(cruiser, [submarine, ["C1", "B1"])
+    ##consecutive coordinates
+    #assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    #assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    #assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    #assert_equal false, board.valid_placement?(cruiser, ["C1", "B1"])
   end
+
+  def test_consecutive_column_coordinates
+    board = Board.new
+    cruiser = Ship.new('Cruiser', 3)
+    submarine = Ship.new('Submarine', 2)
+
+    assert_equal false, board.consecutive_column_coordinates(cruiser, ["A1", "A3", "A2"])
+
+  end
+
+
+
+
+  #def test_convert_coordinates
+    #board = Board.new
+#
+    #cruiser = Ship.new('Cruiser', 3)
+    #submarine = Ship.new('Submarine', 2)
+    #board.convert_coordinates(cruiser, ["A1", "A2", "A3"])
+#
+#binding.pry
+#
+    #assert_equal [[65, 49], [65, 50], [65, 51]], board.convert_coordinates(cruiser, ["A1", "A2", "A3"])
+  #end
+#
+  #def test_ordinal_difference
+#
+    #board = Board.new
+    #cruiser = Ship.new('Cruiser', 3)
+    #submarine = Ship.new('Submarine', 2)
+    #board.convert_coordinates(cruiser, ["A1", "A2", "A3"])
+#
+    #assert_equal true, board.ordinal_difference
+  #end
+
+
+
 end
