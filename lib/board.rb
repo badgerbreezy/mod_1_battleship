292af988cc50f59c2a771ea904385996b4a6291a
todd-estes <elestes@gmail.com>
2020-08-29 22:41:43 -0600

    Update render method

--- lib/board.rb
+++ lib/board.rb
@@ -22,7 +22,7 @@ class Board
   end
 
   def valid_coordinate?(coordinate)
-    @grid.value?(grid[coordinate]) 
+    @grid.value?(grid[coordinate])
   end
 
   def valid_placement?(ship, ship_location)
@@ -120,4 +120,24 @@ class Board
       end
     end
   end
+
+  def render(answer = false)
+    column_range = 1..4
+    row_range = "A".."D"
+    board_rows = []
+    top_row ="  " + "1 2 3 4" + " \n"
+    row_range.each do |row|
+      row_string = row + " "
+      # ["A ", "B ","C ","D "]
+      column_range.each do |column|
+        cell_coordinate = row + column.to_s
+        # ["A1", "A2",..."D4"]
+        row_string += @grid[cell_coordinate].render(answer) + " "
+        # ["A . .  .  . \n", "B . . . . \n"","C . . . \n,"D . . . .\n"]
+      end
+      board_rows << row_string + "\n"
+    end
+    puts top_row + board_rows.join
+  end
+end
 end
