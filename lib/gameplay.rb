b449c56cf62fc20a27a606566d9623f51ad42da8
todd-estes <elestes@gmail.com>
2020-08-31 18:17:25 -0600

    Add gameplay.rb

---
+++ lib/gameplay.rb
@@ -0,0 +1,75 @@
+class Gameplay
+  attr_reader :computer, :player
+  def initialize(computer, player)
+    @computer = computer
+    @player = player
+  end
+
+  def menu
+  puts "Welcome to BATTLESHIP"
+  puts "Enter p to play. Enter q to quit."
+  ">"
+  reply = gets.chomp.downcase
+    if reply == 'p'
+      set_up
+    elsif reply == 'q'
+      menu
+    end
+  end
+
+  def set_up
+    @computer.generate_random_possible_ship_placements(3)
+    @computer.generate_random_possible_ship_placements(2)
+    puts "I have laid out my ships on the grid."
+    puts "You now need to lay out your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    @player.board.render
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    puts ">"
+    entry_1 = gets.chomp.upcase.split(" ")
+    # require "pry"; binding.pry
+    # player_crusier = Ship.new("cruiser", 3 )
+    until @player.board.valid_coordinate?(entry_1[0]) == true && @player.board.valid_coordinate?(entry_1[1]) == true && @player.board.valid_coordinate?(entry_1[2])
+    puts "Those are invalid coordinates. Please try again:"
+    puts ">"
+    entry_1 = gets.chomp.upcase.split(" ")
+    end
+    player_cruiser = Ship.new("cruiser", 3 )
+    @player.board.place(player_cruiser, entry_1)
+    p "Enter the squares for the Submarine (2 spaces)"
+    p ""
+    #ends here
+    turn
+  end
+
+  def turn
+    until computer.has_lost? == true || player_1.has_lost? == true
+      p "=============COMPUTER BOARD============="
+      computer.board.render
+      p "==============PLAYER BOARD=============="
+      player.board.render
+      player_shot_process
+      computer_shot_process
+    end
+  end
+
+  def shot_process
+    until valid_coordinate(shot_entry) == true
+      p "Enter the coordinate for your shot:"
+      ">"
+      shot_entry = gets.chomp.upcase.to_a
+    if board.valid_coordinate(shot_entry)
+      take_shot
+    else
+      p "Please enter a valid coordinate:"
+      p ">"
+    end
+  end
+
+  def computer_shot_process
+    until player.board.computer_shot
+      computer_shot = player.board.grid.sample(1)
+    end
+  end
+end
+end
